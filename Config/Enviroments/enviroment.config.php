472428e0613aeb67be4cb6a4c3c1cb681651d485
duongtuananh <goat91@gmail.com>
2016-11-10 15:35:56 +0700

    config & htacrss

--- Config/Enviroments/enviroment.config.php
+++ Config/Enviroments/enviroment.config.php
@@ -1,9 +1,18 @@
 <?php
 
-switch ($_SERVER['HTTP_HOST']) {
-    case 'production.tamviettech.vn':
-        $exports = 'production';
-        break;
-    default:
-        $exports = 'development';
+$config = "development.config.php";
+
+foreach (scandir(__DIR__) as $item)
+{
+    if ($item == 'enviroment.config.php' || $item == 'development.config.php' || strpos($item, '.config.php') === false)
+    {
+        continue;
+    }
+
+    $config = $item;
+    break;
+}
+
+$exports = str_replace('.config.php', '', $config);
+
 }
